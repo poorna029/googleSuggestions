cb4f80f427e2b7a3a0e8bfc1efbf57460961965d
Poorna <poorna029@gmail.com>
2023-03-25 18:01:11 +0000

    with class componens

--- src/components/GoogleSuggestions/index.js
+++ src/components/GoogleSuggestions/index.js
@@ -1,42 +1,42 @@
 // Write your code here
 import './index.css'
-import {useState} from 'react'
+import {Component} from 'react'
 import {Button} from 'react-bootstrap'
 import SuggestionItem from '../SuggestionItem/index'
 
-const GoogleSuggestions = props => {
-  const {suggestionsList} = props
-  const [search, setSearch] = useState({
-    list: suggestionsList,
-    len: 0,
-    suggestion: '',
-  })
-  const fn = e => {
+class GoogleSuggestions extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {list: props.suggestionsList, suggestion: ''}
+  }
+
+  fn = e => {
+    console.log(e.target.value)
     if (e.target.value.length > 0) {
-      const a = suggestionsList.filter(j =>
+      const {list} = this.state
+      const a = list.filter(j =>
         j.suggestion.toLowerCase().includes(e.target.value.toLowerCase()),
       )
+      console.log(e.target.value.length)
       const sg = new Set(e.target.value.toLowerCase().split(''))
 
-      //   const b = console.log(e.target.value.length)
-      //   console.log(a)
       if (a.length < 2) {
         const obj = []
         let obj1 = []
-        suggestionsList.forEach(set => {
+        list.forEach(set => {
           const sn = new Set(set.suggestion)
           const sO = new Set([...sn].filter(x => sg.has(x)))
           obj.push(sO.size)
           obj1 = [...obj]
         })
-        console.log(obj.sort((first, second) => second - first))
-        console.log(obj1, obj)
+        // console.log(obj.sort((first, second) => second - first))
+        // console.log(obj1, obj)
         const firstVal = obj[0]
         const secVal = obj[1]
         const firstValInd = obj1.indexOf(firstVal)
         const newList = []
         let secValInd = obj1.indexOf(secVal)
-        newList.push(suggestionsList[firstValInd])
+        newList.push(list[firstValInd])
         if (firstValInd === secValInd) {
           console.log('same')
           for (let i = 0; i < obj.length; i += 1) {
@@ -46,35 +46,47 @@ const GoogleSuggestions = props => {
             }
           }
         }
-        newList.push(suggestionsList[secValInd])
-        setSearch({list: newList, len: e.target.value.length})
-      } else {
-        setSearch({list: a, len: e.target.value.length})
-      }
 
-      //   console.log(suggestionsList[firstValInd], suggestionsList[secValInd])
+        newList.push(list[secValInd])
+        console.log(newList)
+        this.setState({list: newList, suggestion: e.target.value})
+      } else {
+        this.setState({list: a, suggestion: e.target.value})
+        // console.log(a)
+      }
     } else if (e.target.value.length === 0) {
-      setSearch({list: [], len: 0})
+      const {suggestionsList} = this.props
+      this.setState({list: suggestionsList, suggestion: ''})
     }
   }
-  const bgs = search.list.length > 0 ? 'bgs' : ''
 
-  const f2 = p => {
-    setSearch({...search, suggestion: p})
+  f2 = p => {
+    this.setState({suggestion: p})
   }
 
-  return (
-    <div className="d-flex flex-row align-items-start justify-content-center main bg-white ">
-      <div className="d-flex flex-col justify-content-center align-items-center d ">
+  render() {
+    const {list, suggestion} = this.state
+
+    console.log(list)
+    // this.setState({list: suggestionsList})
+    // console.log(list)
+    // const a = list.filter(j =>
+    //   j.suggestion.toLowerCase().includes(list.toLowerCase()),
+    // )
+    // console.log(a)
+
+    const b = list.length > 0 ? 'p-border' : ''
+    const bor = list.length > 0 ? 'over-border' : ''
+
+    return (
+      <div className="d">
         <img
           src="https://assets.ccbp.in/frontend/react-js/google-logo.png"
           alt="google logo"
           className="img"
         />
-        <div className="d-flex flex-col justify-content-center align-items-center new ">
-          <div
-            className={`d-flex flex-row justify-content-center align-items-center bg ${bgs} m-8`}
-          >
+        <div className="superClass">
+          <div className={`p ${b}`}>
             <img
               src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png"
               alt="search icon"
@@ -84,21 +96,20 @@ const GoogleSuggestions = props => {
               type="search"
               className="inp"
               placeholder="Search Google"
-              onChange={fn}
-              value={search.suggestion}
+              onChange={this.fn}
+              value={suggestion}
             />
           </div>
-          {search.list.length > 0 ? (
-            <ul className="over bg-white ">
-              {search.list.map(e => (
-                <SuggestionItem val={e} val1={f2} key={e.id} />
+          {list.length > 0 ? (
+            <ul className={`over ${bor} border mt-0`}>
+              {list.map(e => (
+                <SuggestionItem val={e} val1={this.f2} key={e.id} />
               ))}
             </ul>
           ) : null}
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
-
 export default GoogleSuggestions
